241b927f4b76372753978a7cd949676e3aeb0f18
leonoxo <alex71.sander71@gmail.com>
2020-02-03 15:14:22 -0300

    add pages and router links and implement  top menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from "react";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MenuTop from "./components/MenuTop";
 
-function App() {
+//Pages
+import Home from "./pages/home";
+import NewMovies from "./pages/new-movies";
+import Movie from "./pages/movie";
+import Search from "./pages/search";
+import Error404 from "./pages/error404";
+import Popular from "./pages/popular";
+
+export default function App() {
+  const { Header, Content } = Layout;
+
   return (
-    <div>
-      <h1>Estamos en app</h1>
-    </div>
+    <Layout>
+      <Router>
+        <Header>
+          <MenuTop></MenuTop>
+        </Header>
+
+        <Content>
+          <Switch>
+            <Route path="/" exact={true}>
+              <Home />
+            </Route>
+            <Route path="/new-movies" exact={true}>
+              <NewMovies />
+            </Route>
+            <Route path="/popular" exact={true}>
+              <Popular />
+            </Route>
+            <Route path="/search" exact={true}>
+              <Search />
+            </Route>
+            <Route path="/movie/:id" exact={true}>
+              <Movie />
+            </Route>
+            <Route path="*" exact={true}>
+              <Error404 />
+            </Route>
+          </Switch>
+        </Content>
+      </Router>
+    </Layout>
   );
 }
-
-export default App;
