5c5fd2c3922143bcf8be96bb13adf494975aad1d
leonoxo <alex71.sander71@gmail.com>
2020-02-03 15:48:05 -0300

    create hook to get movies

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React from "react";
+import useFetch from "../hooks/useFetch";
 
-export default function Home(){
-    return 'Estamos en Home';
+export default function Home() {
+  const url =
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=d229a6095672f117ec1f7246b639291b&language=es-ES&page=1";
+
+  const movies = useFetch(url);
+
+  console.log(movies);
+  return "Estamos en Home";
 }
