c2faa7404f197cf670e3a58710a115ea137eb011
Alexsander <alexsander.zuniga@readinessit.com>
2020-02-06 16:51:48 -0300

    component to display movie information

---
+++ src/pages/movie/movie.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Row, Col, Button } from 'antd';
+import { useParams } from 'react-router-dom'
+import moment from 'moment';
+import useFetch from '../../hooks/useFetch';
+import { url_API, API } from '../../utils/constants';
+import Loading from '../../components/Loading';
+
+import './movie.scss';
+
+export default function Movie(){
+
+    const { id } = useParams();
+
+    const movieInfo = useFetch(
+        `${url_API}/movie/${id}?api_key=${API}&language=es-ES`
+    );
+
+    if(movieInfo.loading && !movieInfo.result){
+        return <Loading />;
+    }
+
+    return (
+        <RenderMovie movie={movieInfo.result} />
+    );
+}
+
+function RenderMovie(props) {
+
+    const { movie: { title, backdrop_path, poster_path } } = props;
+
+    const backdropPath = `https://image.tmdb.org/t/p/original/${backdrop_path}`;
+    
+
+    return (
+        <div className='movie' style={{ backgroundImage: `url('${backdropPath}')`}}>
+            <div className='movie__dark'></div>
+            <Row>
+                <Col span={8} offset={3} className='movie__poster' >
+                    <PosterMovie imagen={poster_path} />
+                </Col>
+                <Col span={10} className='movie__info' >
+                    <MovieInfo movieInfo={props.movie} />
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+function PosterMovie (props){
+    const {imagen} = props
+
+
+    const posterPath = `https://image.tmdb.org/t/p/original/${imagen}`
+
+    return (
+        <div style={{ backgroundImage: `url('${posterPath}')`}}/>
+    )
+}
+
+function MovieInfo(props){
+    const {movieInfo: {id, title, release_date, overview, genres}} = props
+    
+
+    return (
+        <>
+            <div className='movie__info-header'>
+                <h1> {title} <span>{moment(release_date, 'YYYY-MM-DD').format('YYYY')}</span></h1>
+                <button>Ver Trailer</button>
+            </div>
+            <div className='movie__info-content' >
+                <h3>Información general</h3>
+                <p>{overview}</p>
+
+                <h3>Generos</h3>
+                <ul>
+                    {genres.map(gender => (
+                        <li key={gender.id}>{gender.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+}
